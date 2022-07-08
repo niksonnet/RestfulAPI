c3260a65717b219708b1bba72f669076ca5d592f
niksonnet <nikunjkumar.patel@innroad.com>
2022-07-08 16:06:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const PORT = 3000;
+
+const app = express();
+app.use(helmet());
+
+app.get('/', (req, res) => {
+  console.log(req);
+  res.send("<h1>Express</h1>")
+})
+app.listen(PORT, () => {
+  console.log(`listening on
+   ${PORT}`);
+})
+
